a39e80accf011ceb1a7f805ac84108a048d6969f
Gibexz <andrewetedu@gmail.com>
2023-10-13 01:16:42 +0100

    Task:7. Average score fix1

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,17 @@
 -- computes and store the average score for a student.
 
+drop procedure if exists ComputeAverageScoreForUser;
 delimiter //
-
-create procedure ComputeAverageScoreForUser(in user_id int)
+create procedure ComputeAverageScoreForUser(in _id int)
 begin
     declare avg_score float;
     select avg(score) into @avg_score
     from corrections
-    where user_id = user_id;
-    update users set average_score = @avg_score where id = user_id;
+    where user_id = _id;
+
+    update users
+    set average_score = @avg_score
+    where id = _id;
 end;
 //
 
