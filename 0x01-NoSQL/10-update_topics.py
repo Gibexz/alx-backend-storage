dad216e05764da2f1c5343c897bec0c7552d2320
Gibexz <andrewetedu@gmail.com>
2023-10-17 19:00:27 +0100

    Task 10. Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module: 10-update_topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Python function that changes all topics of a school document
+    based on the name
+    """
+    if mongo_collection is not None:
+        mongo_collection.update_one(
+            {"name": name},
+            {"$set": {"topics": topics}}
+        )
